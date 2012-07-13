642f9c31d101ad7ef1050f05589ec2d16c952e19
Jack Franklin <jack@jackfranklin.net>
2012-07-13 18:04:30 +0100

    Initial structure set up and first passing test!

---
+++ src/jquery.formbuilder.js
@@ -0,0 +1,22 @@
+/**
+ * jQuery Form Builder
+ * Written by Jack Franklin and Ben Everard
+ *
+ * https://github.com/jackfranklin/jquery-form-builder
+ **/
+
+(function(window) {
+  var jFB = {
+
+    init: function(json) {
+      return this;
+    }
+
+
+  };
+
+  window.FormBuilder = function(json) {
+    return jFB.init(json);
+  };
+
+})(window);
---
+++ test/spec/formbuilderSpec.js
@@ -0,0 +1,6 @@
+describe("jQuery Form Builder", function() {
+
+  it("sets up the global FormBuilder method", function() {
+    expect(window.FormBuilder()).toBeTruthy()
+  });
+});
