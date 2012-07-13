b30c7fe82ca8ed47c934bd17e7de67f654d044ad
Jack Franklin <jack@jackfranklin.net>
2012-07-14 00:11:56 +0100

    Basic validations work! Needs a lot of refactoring.
    But good to see a very early prototype working

--- src/jquery.formbuilder.js
+++ src/jquery.formbuilder.js
@@ -56,7 +56,7 @@
         //set up the formField object
         formFields[value.name] = {
           //give them a reference to the actual HTML
-          html: $(fieldElement).attr(value),
+          html: $(document.createElement(fieldElement)).attr(value),
           //and all the attributes
           attributes: value
         };
@@ -70,10 +70,52 @@
     };
 
 
+    //TODO: only manages validations with just one parameter
+    var validate = function(name, validations) {
+      var field = formFields[name];
+      console.log(field);
+      if (!field) return false
+
+      var vals = splitValidations(validations);
+      console.log(vals);
+      //only one validation, no need to loop
+      if(vals.length === 1) {
+        var validation = vals[0];
+        var validationMethod = validations.split("(")[0];
+        console.log("field", field.attributes);
+        return validationMethods[validationMethod](field.html, extractParams(validation));
+      } else {
+        //TODO: test, rewrite, etc. This is a load of crap atm
+        for(var i = 0; i < vals.length; i++) {
+          var validation = vals[i];
+          console.log(validation);
+          console.log(extractParams(validation));
+          validationMethods[validation](field.html, extractParams(validation));
+        };
+      }
+
+    };
+
+
+    var splitValidations = function(validations) {
+      if(validations.indexOf("|") > -1) {
+        return validations.split("|");
+      } else {
+        return [validations];
+      }
+    };
+
+    var extractParams = function(validation) {
+      //TODO must be a better way - regex to extract every thing within a bracket separated by a , - eg from (5, 4, 2) pull out [5,4,2]
+      //this hacky solution only works for one parameter
+      var params = validation.split("(");
+      var param = params[1].split(")");
+      return param[0];
+    };
 
     //object that we store all the validations in - these are not passed to the API
     //written this just as an idea of how they work - this doesn't actually do anything yet
-    var validations = {
+    var validationMethods = {
       min_length: function(obj, x) {
         return $(obj).val().length > x;
       }
@@ -84,7 +126,8 @@
     return {
       init: init,
       generate: generate,
-      fields: fields
+      fields: fields,
+      validate: validate
     };
   })();
 
--- test/spec/formbuilderSpec.js
+++ test/spec/formbuilderSpec.js
@@ -4,6 +4,7 @@ describe("jQuery Form Builder", function() {
   beforeEach(function() {
     testData.validJson = '{ "name" : "Jack Franklin", "Age" : 20 }';
     testData.invalidJson = '{ "Name" : Jack Franklin }';
+    testData.formJson = '{ "fields" : [{ "element": "input", "type": "text", "class": "testClass", "name": "username" }]}';
   });
 
 
@@ -22,10 +23,33 @@ describe("jQuery Form Builder", function() {
 
   describe("generating the fields", function() {
     it("can parse out the attributes for each input", function() {
-      var formJson = '{ "fields" : [{ "element": "input", "type": "text", "class": "testClass", "name": "username" }]}';
-      var formData = FormBuilder(formJson);
+      var formData = FormBuilder(testData.formJson);
       formData.generate();
       expect(formData.fields("username").attributes.class).toEqual("testClass");
     });
+  });
+
+  describe("validation", function() {
+    var validationTest = {};
+    beforeEach(function() {
+      validationTest.formData = FormBuilder(testData.formJson);
+      validationTest.formData.generate();
+    });
+
+    describe("min_length", function() {
+      it("returns true for fields that are of the minimum length", function() {
+        $(validationTest.formData.fields("username").html).val("jackfranklin");
+        expect(validationTest.formData.validate("username", "min_length(5)")).toEqual(true);
+      });
+
+      it("returns false for fields that are below minimum length", function() {
+        $(validationTest.formData.fields("username").html).val("j");
+        expect(validationTest.formData.validate("username", "min_length(5)")).toEqual(false);
+
+      });
+
+    });
+
+
   });
 });
