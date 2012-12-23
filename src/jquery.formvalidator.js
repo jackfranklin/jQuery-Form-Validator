92d0cefee6792471fa09e8a8b60f7b5ac814353b
Jack Franklin <jack@jackfranklin.net>
2012-12-23 22:57:29 +0000

    Update comments to generate a more useful Docco build

--- src/jquery.formvalidator.js
+++ src/jquery.formvalidator.js
@@ -1,9 +1,11 @@
-/**
+/*
  * jQuery Form Validator
  * Written by Jack Franklin
  *
  * https://github.com/jackfranklin/jQuery-Form-Validator
- **/
+*/
+
+// jQuery Form Validator. [Usage Documentation on Github](https://github.com/jackfranklin/jQuery-Form-Validator)
 
 
 /** getAttributes plugin
@@ -21,21 +23,25 @@
 })(jQuery);
 
 (function(window) {
+  // the JFV class
   var JFV = function() {
     this.VERSION = "0.6.0";
-    // store all our fields
+
+    // Stores all the form fields that are added to the JFV
     this.formFields = {};
 
-    // object to store pending validations
+    // Object to store pending validations that have yet to be run
     this.pendingValidations = {};
 
-    // init method - fields can be passed in on init
+    // This is called automatically when you run `window.FormValidator();`
+    // You can pass in fields that will be added to the JFV instance for you;
+    // For example: `window.formValidator($("input"))`;
     this.init = function(fields) {
       if(fields) { this.addFields(fields); }
       return this;
     };
 
-    //adds an individual field to the list
+    // Add an individual field to the JFV object
     this.addField = function(field) {
       var attrs = $(field).getAttributes();
       this.formFields[attrs.name] = {
@@ -44,33 +50,35 @@
       };
     };
 
-    // loops over a jQ set, adding all fields to the form
+    // Loops over the given jQ set, adding all fields to the form
     this.addFields = function(fields) {
       for(var i = 0; i < fields.length; i++) {
         this.addField(fields[i]);
       }
     };
 
-    //returns the object for a form element, based off its name attribute
+    // Returns the object for a form element, based off its name attribute
     this.field = function(name) {
       return this.formFields[name];
     };
 
-    // clears out the form fields
+    // Clears out all the fields the JFV validator knows about.
+    // Useful if you need to completely clear all the fields
     this.clearFields = function() {
       this.formFields = {};
     };
 
-    //validates a field against validation method(s)
-    //validateField("username", { max_length: 6, required: true })
+    // Validates a field against validation method(s). For example:
+    // `validateField("username", { max_length: 6, required: true });`
     this.validateField = function(name, validations) {
-
       var field = this.formFields[name];
       var fieldType = field.html.attr("type");
       var fieldValue;
+      // make sure we have `fieldValue` set to the right value.
+      // For checkboxes and radio buttons, val() isn't the way to do things.
+
+      // TODO: deal with all types, and make this more robust
       if(field.html) {
-        // deal with other field types
-        // TODO: deal with more than just text and checkboxes
         if(fieldType !== "text") {
           if(fieldType === "checkbox" || fieldType === "radio") {
             fieldValue = field.html.is(":checked");
@@ -80,28 +88,31 @@
         }
       }
 
-      //if we dont have a field then just exit out of this one
+      // If we don't have a field, just return false here
       if (!field) { return false; }
 
-      //store the validation error messages
+      // Store any error messages we get from the validations
       var errorMessages = [];
 
-      // method to run all the validations on the value
+      // Runs the field value against every validation passed in
       for(var validation in validations) {
         var method = this.getValidationMethod(validation);
         var params = validations[validation];
 
         if(!method) { throw new Error("Validation method " + validation + " does not exist"); }
 
+        // If it doesn't pass, store the error message, otherwise do nothing.
         if(!method.fn(fieldValue, params, field.html)) {
           errorMessages.push(this.replacePlaceholdersInMessage(method.message, { name: name, params: params }));
         }
       }
 
+      // return the final validation object.
       return { valid: !(errorMessages.length), field: field, messages: errorMessages };
     };
 
-    // replaces placeholders in a validation message with the actual data
+    // Replaces placeholders in a validation message with the actual data.
+    // `data` contains the field name and the fields parameters
     this.replacePlaceholdersInMessage = function(message, data) {
       // substitute %F for the field name
       message = message.replace(/%F/g, data.name);
@@ -109,28 +120,28 @@
       var dataParams = data.params;
       // if it is an array of multiple params, we loop through and replace each
       if( Object.prototype.toString.call(dataParams) === '[object Array]' ) {
-        //array of multiple parameters
         var dataParamsLen = dataParams.length;
         for(var i = 0; i < dataParamsLen; i++) {
-          // substitute %ARGS[0], %ARGS[1], etc with the actual parameter
+          // Substitute %ARGS[0], %ARGS[1], etc with the actual parameter
           message = message.replace(new RegExp('%ARGS\\['+i+'\\]', "g"), data.params[i]);
         }
       } else {
-        //just one so substitute %ARG[0] for it
+        // Just one parameter so substitute %ARG[0] for it
         message = message.replace(/%ARG/g, dataParams);
       }
       return message;
     };
 
-    // method to return pending validations
+    // Method to return pending validations
     this.getPendingValidations = function() {
       return this.pendingValidations;
     };
 
-    //method for stacking validations
+    // Method for stacking validations, that is, adding methods without running them.
+    // Useful for programatically adding validation methods before running them all
     this.addValidation = function(fieldName, validations) {
       if(this.pendingValidations[fieldName]) {
-        //some already exist, so loop through and apply the new ones onto the existing object
+        // Some already exist, so loop through and apply the new ones onto the existing object
         for(var newValidation in validations) {
           this.pendingValidations[fieldName][newValidation] = validations[newValidation];
         }
@@ -139,38 +150,42 @@
       }
     };
 
-    //method for clearing pending validations
+    // Method for clearing pending validations
     this.clearPendingValidations = function() { this.pendingValidations = {}; };
 
-    //method for running validations
+    // Method for running all the pending validations
     this.runValidations = function(clearAfter) {
       //ensure it's boolean true or false
       clearAfter = !!clearAfter;
 
-      // will form part of the response
+      // Will form part of the response
       var fields = {};
-      var isValid = true;
 
+      // Loop through every field, and run the validations on it.
       for(var field in this.pendingValidations) {
-        //validate the field
         var resp = this.validateField(field, this.pendingValidations[field]);
+        // Store the response for this field to the fields object.
         fields[field] = { field: resp.field, messages: resp.messages, valid: resp.valid, html: resp.field.html };
       }
       if(clearAfter) { this.clearPendingValidations(); }
+      // `getAllErrors` loops through the error messages for each individual field,
+      //  and concatenates them all into one large array
       var allErrors = this.getAllErrors(fields);
       return { valid: !allErrors.length, fields: fields, messages: this.getAllErrors(fields) };
     };
 
-    /*fields object looks like:
-     * var fields = {
-     *    username: {
-     *      field: [ jQuery obj],
-     *      messages: [ array of error messages ],
-     *      valid: true/false // if that field passed its validations
-     *      }
-     *    }
-     */
+    // Loops through the error messages for each individual field,
+    // and concatenates them all into one large array
     this.getAllErrors = function(fieldsObj) {
+      /*fields object looks like:
+       * var fields = {
+       *    username: {
+       *      field: [ jQuery obj],
+       *      messages: [ array of error messages ],
+       *      valid: true/false // if that field passed its validations
+       *      }
+       *    }
+       */
       var allErrors = [];
       for(var field in fieldsObj) {
         allErrors = allErrors.concat(fieldsObj[field].messages);
@@ -186,7 +201,7 @@
      * object = jQuery ref to field
     **/
 
-    //TODO: use add_validation method rather than directly adding them here?
+    //TODO: use `add_validation` method rather than directly adding them here?
     this.validationMethods = {
       min_length: {
         message: "Field %F must be at least length %ARG",
@@ -206,7 +221,6 @@
           return (val !== "" && !!val);
         }
       },
-      //this takes two arguments, the min and max length, so the arguments here are an array.
       length_between: {
         message: "Field %F must be a minimum of %ARGS[0] characters and a maximum of %ARGS[1]",
         fn: function(val, args) {
@@ -222,22 +236,24 @@
       }
     };
 
-    // adds a new validation method
+    // Adds a new validation method
+    // This also will override an existing method, if you want to change how a method works
     this.addValidationMethod = function(name, obj) {
       this.validationMethods[name] = obj;
     };
 
+    // Grab a validation method object, if you want to alter it and then save it back.
     this.getValidationMethod = function(name) {
       return this.validationMethods[name];
     };
   };
 
-  // expose globally
+  // Expose globally.
   window.FormValidator = function(fields) {
     return new JFV().init(fields);
   };
 
-  // support AMD like RequireJS
+  // Support AMD libraries, such as RequireJS
   if(typeof define === 'function') {
     define([], function() {
       return window.FormValidator;
